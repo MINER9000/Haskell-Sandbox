ac6ea7cfd4c147eb3a979b2dbaa7739e56c24be0
Daniil <suver.www@mail.ru>
2019-02-11 15:33:40 +0300

    Create Integration.hs
    integration function

---
+++ Integration.hs
@@ -0,0 +1,8 @@
+module Integration where
+
+integration :: (Double -> Double) -> Double -> Double -> Double
+integration f a b = (h a b) * ((f a + f b) / 2 + summ a b)
+  where summ a b = (foldr1 ((+) . f . f) (tail $ init [a, a + (h a b) .. b]))
+
+h :: Double -> Double -> Double
+h a b = (b - a) / 10000.0
