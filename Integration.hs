28846f51461134b7b78b31b7fd3b26c7cb0a208b
miner9000 <wertufan@mail.ru>
2019-02-13 17:11:37 +0300

    Working on integration

--- Integration.hs
+++ Integration.hs
@@ -1,8 +1,8 @@
 module Integration where
 
-integration :: (Double -> Double) -> Double -> Double -> Double
+integration :: (Float -> Float) -> Float -> Float -> Float
 integration f a b = (h a b) * ((f a + f b) / 2 + summ a b)
-  where summ a b = sum $ map f (tail $ init [a, a + (h a b) .. b])
+  where summ a b = sum $ map f [a, a + (h a b) .. b]
 
-h :: Double -> Double -> Double
-h a b = (b - a) / 10000.0
+h :: Float -> Float -> Float
+h a b = (b - a) / 1000.0
